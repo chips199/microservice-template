7c25405fbd472163cf01302d1b2b322564b781ad
Marcel Scheck <marcel.scheck@hetzner-cloud.de>
2022-01-17 21:48:25 +0100

    Add embedded MongoDB, integration test for persistence and docker-compose.yml

--- src/main/java/de/provadis/microservicetemplate/adapters/out/persistence/TodoListEntity.java
+++ src/main/java/de/provadis/microservicetemplate/adapters/out/persistence/TodoListEntity.java
@@ -3,9 +3,11 @@ package de.provadis.microservicetemplate.adapters.out.persistence;
 import de.provadis.microservicetemplate.domain.Task;
 import de.provadis.microservicetemplate.domain.TodoList;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Document("todos")
 public class TodoListEntity {
     @Id
     public String id;
